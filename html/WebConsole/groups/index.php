82fa6c554474b73d438cd9bbcaa04e70eebdecec
Aluminum-Fairy <Aluminum-Fairy@users.noreply.github.com>
2022-01-13 05:05:10 +0000

    php-cs-fixer by Github Actions

--- html/WebConsole/groups/index.php
+++ html/WebConsole/groups/index.php
@@ -13,6 +13,6 @@ $groupId = explode('/', $matches[1])[0];
 
 $requestMethod=strtolower($_SERVER["REQUEST_METHOD"]);
 
-if ($requestMethod === "get"){
+if ($requestMethod === "get") {
     require(__DIR__."/getGroupList.php");
 }
