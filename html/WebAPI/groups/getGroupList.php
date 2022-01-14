642514400027beb6fdb32a374ab0e890003b562e
Aluminum-Fairy <Aluminum-Fairy@users.noreply.github.com>
2022-01-14 01:27:30 +0000

    php-cs-fixer by Github Actions

--- html/WebAPI/groups/getGroupList.php
+++ html/WebAPI/groups/getGroupList.php
@@ -6,9 +6,9 @@ require_once(__DIR__ . "/../../../lib/UserGroup.php");
 require_once(__DIR__."/../../../lib/Sensor.php");
 $UserGroup = new UserGroup($loginInfo);
 $Sensor = new Sensor($loginInfo);
-if(($result = $UserGroup->getGroupName($groupId)) !==false){
+if (($result = $UserGroup->getGroupName($groupId)) !==false) {
     $result += array("users"=>$Sensor->getAllowedGroupUsersDiscvList($groupId));
     echo json_encode($result);
-}else{
+} else {
     http_response_code(400);
 }
