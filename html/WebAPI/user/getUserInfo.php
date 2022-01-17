9354047e7d852788c73f98536ff7b6cc044f19ef
Aluminum-Fairy <miyamoto2525family0731@gmail.com>
2022-01-17 15:23:49 +0900

    userAPIのpublicationPlace部分完成

--- html/WebAPI/user/getUserInfo.php
+++ html/WebAPI/user/getUserInfo.php
@@ -12,5 +12,6 @@ $userId = 1;
 if ($userId !== false) {
     $result = $UserInfo->getViewDays($userId);
     $result += $UserInfo->getViewTime($userId);
+    $result +=$UserInfo->getViewSensorConfig($userId);
     echo json_encode($result);
 }
