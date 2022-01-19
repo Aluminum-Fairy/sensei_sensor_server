0d066191902dcaad59a71723db3470e8189885b3
Aluminum-Fairy <miyamoto2525family0731@gmail.com>
2022-01-19 11:07:36 +0900

    preflight対策をログイン処理に追加(Optionsリクエストはexit())

--- WebAPI/login/index.php
+++ WebAPI/login/index.php
@@ -6,5 +6,9 @@ require_once(__DIR__ . "/../../lib/JwtAuth.php");
 require_once(__DIR__ . "/../../config/Config.php");
 
 $JWT = new JwtAuth($loginInfo);
+$requestMethod = strtolower($_SERVER["REQUEST_METHOD"]);
+if ($requestMethod === "options"){
+    exit();
+}
 
 $JWT->login();
