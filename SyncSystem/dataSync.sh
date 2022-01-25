02fa668a1a89da8b4c045c41ab0b7cbf62e8ad61
Aluminum-Fairy <miyamoto2525family0731@gmail.com>
2022-01-25 13:23:56 +0900

    同期スクリプト修正

--- SyncSystem/dataSync.sh
+++ SyncSystem/dataSync.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "{" >>  ~/SyncMessageLog.log
 date >> ~/SyncMessageLog.log
-php /var/www/html/sensei-sensor-php/SyncSystem/syncUserList.log >> ~/SyncMessageLog.log
-php /var/www/html/sensei-sensor-php/SyncSystem/syncTagList.log >> ~/SyncMessageLog.log
-php /var/www/html/sensei-sensor-php/SyncSystem/syncSensorList.log >> ~/SyncMessageLog.log
-php /var/www/html/sensei-sensor-php/SyncSystem/syncDiscvLog.log >> ~/SyncMessageLog.log
+php /var/www/html/sensei-sensor-php/SyncSystem/syncUserList.php >> ~/SyncMessageLog.log
+php /var/www/html/sensei-sensor-php/SyncSystem/syncTagList.php >> ~/SyncMessageLog.log
+php /var/www/html/sensei-sensor-php/SyncSystem/syncSensorList.php >> ~/SyncMessageLog.log
+php /var/www/html/sensei-sensor-php/SyncSystem/syncDiscvLog. php >> ~/SyncMessageLog.log
 echo "}," >> ~/SyncMessageLog.log
